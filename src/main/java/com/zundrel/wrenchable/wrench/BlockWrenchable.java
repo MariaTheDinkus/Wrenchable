5c6d07142616833deb7c8fc444f8e3c0bc3d25d1
Jacob Farley <jacobfarley2017@gmail.com>
2019-09-03 17:34:09 -0500

    Fix JavaDocs being incorrect for BlockWrenchable and EntityWrenchable.

--- src/main/java/com/zundrel/wrenchable/wrench/BlockWrenchable.java
+++ src/main/java/com/zundrel/wrenchable/wrench/BlockWrenchable.java
@@ -6,7 +6,7 @@ import net.minecraft.world.World;
 
 public interface BlockWrenchable {
     /**
-     * This method is called when a block is wrenched. Durability handling should be done in this method.
+     * This method is called when a block is wrenched.
      * @param world An instance of the world where this block was wrenched.
      * @param player The player who wrenched this block.
      * @param result Information about the block that was wrenched.
