f799080c1ca92dbc454d9c8b98f720631188695c
Jacob Farley <jacobfarley2017@gmail.com>
2019-09-04 20:14:58 -0500

    Added support for swapping the location of a slab.

--- src/main/java/com/zundrel/wrenchable/WrenchableMain.java
+++ src/main/java/com/zundrel/wrenchable/WrenchableMain.java
@@ -3,9 +3,7 @@ package com.zundrel.wrenchable;
 import com.zundrel.wrenchable.block.InstanceListener;
 import com.zundrel.wrenchable.block.PropertyListener;
 import com.zundrel.wrenchable.block.defaults.*;
-import com.zundrel.wrenchable.wrench.WrenchListener;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,9 +14,10 @@ public class WrenchableMain implements ModInitializer {
     public static InstanceListener SIGN_LISTENER;
     public static InstanceListener END_PORTAL_FRAME_LISTENER;
 
-    public static PropertyListener ROTATION_LISTENER;
     public static PropertyListener FACING_LISTENER;
     public static PropertyListener HORIZONTAL_FACING_LISTENER;
+    public static PropertyListener ROTATION_LISTENER;
+    public static PropertyListener SLAB_TYPE_LISTENER;
 
 	@Override
 	public void onInitialize() {
@@ -28,11 +27,13 @@ public class WrenchableMain implements ModInitializer {
 
         END_PORTAL_FRAME_LISTENER = Registry.register(WrenchableRegistry.BLOCK_INSTANCE_LISTENERS, new Identifier(MODID, "eye"), new EndPortalFrameInstanceListener());
 
-	    ROTATION_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "rotation"), new RotationPropertyListener());
-
-	    FACING_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "facing"), new FacingPropertyListener());
+        FACING_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "facing"), new FacingPropertyListener());
 
         HORIZONTAL_FACING_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "horizontal_facing"), new HorizontalFacingPropertyListener());
+
+	    ROTATION_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "rotation"), new RotationPropertyListener());
+
+	    SLAB_TYPE_LISTENER = Registry.register(WrenchableRegistry.PROPERTY_LISTENERS, new Identifier(MODID, "slab_type"), new SlabTypePropertyListener());
 
         WrenchableEvents.init();
 	}
---
+++ src/main/java/com/zundrel/wrenchable/block/defaults/SlabTypePropertyListener.java
@@ -0,0 +1,37 @@
+package com.zundrel.wrenchable.block.defaults;
+
+import com.zundrel.wrenchable.block.PropertyListener;
+import grondag.fermion.modkeys.api.ModKeys;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.enums.SlabType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class SlabTypePropertyListener extends PropertyListener {
+    public SlabTypePropertyListener() {
+        super(Properties.SLAB_TYPE);
+    }
+
+    @Override
+    public void onWrenched(World world, PlayerEntity player, BlockHitResult result) {
+        BlockPos pos = result.getBlockPos();
+        BlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
+
+        if (ModKeys.isControlPressed(player)) {
+            if (state.get(Properties.SLAB_TYPE) == SlabType.BOTTOM) {
+                world.setBlockState(pos, state.with(Properties.SLAB_TYPE, SlabType.TOP));
+                world.updateNeighbor(pos, block, pos);
+                return;
+            } else if (state.get(Properties.SLAB_TYPE) == SlabType.TOP) {
+                world.setBlockState(pos, state.with(Properties.SLAB_TYPE, SlabType.BOTTOM));
+                world.updateNeighbor(pos, block, pos);
+                return;
+            }
+        }
+    }
+}
